94c42eefc6d31d0f950942539258f2c452e8556c
gabrielsaimo <gabrielsaimo68@gmail.com>
2020-11-19 21:27:29 -0300

    navibar 90%

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="css/estrutura.css" type="text/css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class= "container-fluid">
+        <div class= "row linhaUmIndex pb-5 pt-5">
+            <div class="container">
+                <div class= "row">
+                    <div class= "col-3">
+                        <img src="img/logotest.png" alt="img test" title="titleimgtest" width="220">
+                    </div>
+                    <?php
+                    include "menu.php";
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="jquery.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.js" type="text/javascript" ></script>
+    <script src="js/bootstrap.bundle.js" type = "text/javascript" ></script>
+  </body
---
+++ index_modelo.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="jquery.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.js" type="text/javascript" ></script>
+    <script src="js/bootstrap.bundle.js" type = "text/javascript" ></script>
+  </body
